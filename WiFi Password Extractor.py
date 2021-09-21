31a7d6fc015f848dbce922aa834b0bd15afcd8ea
Sina.f <85402429+sina-programer@users.noreply.github.com>
2021-09-21 01:36:53 -0600

    Update something

--- WiFi Password Extractor.py
+++ WiFi Password Extractor.py
@@ -5,8 +5,8 @@ from sys import stdout
 import subprocess
 import os
 
-loading_chars = iter(cycle('/-\|'))
 colors = iter(cycle(['blue', 'red']))
+loading_chars = iter(cycle('/-\|'))
 os.system('cls')
 
 print('\n Welcome to WiFi Password Extractor(written by Sina.f)\n')
@@ -20,20 +20,21 @@ sleep(1)
 print('\n\n\n {:<30}|  Password\n'.format('Host name'))
 output = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode().split('\n')
 
-profiles = [i.split(':')[1][1:-1] for i in output if 'All User Profile' in i]
+profiles = [i.split(':', 1)[1][1:-1] for i in output if 'All User Profile' in i]
 
 for profile in profiles:
     result = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', profile, 'key=clear']).decode('ISO-8859-1').split('\n')
     color = next(colors)
     
     try:
-        password = [b.split(':')[1][1:-1] for b in result if 'Key Content' in b][0]
+        password = [b.split(':', 1)[1][1:-1] for b in result if 'Key Content' in b][0]
         cprint(f' {profile:<30}|  {password}', color=color)
         
     except:
         cprint(f' {profile:<30}|  <No password>', color=color)
         
-    sleep(.8)
+    finally:
+        sleep(.7)
 
 figlet =  f'''\n
    _____ _                __ 
